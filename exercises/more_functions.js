ee6a3490aa437d6be9d419026d342840ade12ce0
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-15 15:21:48 -0400

    add numbers

---
+++ exercises/more_functions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function addNumbers(num1, num2) {
+  let sum = num1 + num2;
+  //   console.log(num1, "+", num2, "=", sum);
+  console.log(num1 + " + " + num2 + " = " + sum);
+}
+
+addNumbers(7, 2);
