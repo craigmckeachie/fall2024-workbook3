522571ee208f01898d06320bcf927f2b9d63f657
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-16 15:13:24 -0400

    escaping characters

--- demos/string-quotes.js
+++ demos/string-quotes.js
@@ -1,7 +1,9 @@
 "use strict";
 
-let quote1 = "In Texas, we use the word ya'll often"; 
+let quote1 = "In Texas, we use the word ya'll often";
 let quote2 = 'Big Tex (at the State Fair) says "Howdy"';
 
+let quote3 = "In Texas, we use the word ya'll often";
+let quote4 = 'Big Tex (at the State Fair) says "Howdy"';
 
-
+console.log(quote3);
