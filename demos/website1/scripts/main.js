4330eef2bd852c372914c20b85b600f30f5e7cba
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-17 13:23:04 -0400

    radio button example

--- demos/website1/scripts/main.js
+++ demos/website1/scripts/main.js
@@ -2,5 +2,18 @@
 "use strict";
 
 let importantParagraph = document.getElementById("importantParagraph");
-importantParagraph.style.backgroundColor = "yellow"
+importantParagraph.style.backgroundColor = "yellow";
 console.log(importantParagraph);
+
+let bowlRadio = document.getElementById("bowlRadio");
+let burritoRadio = document.getElementById("burritoRadio");
+
+function placeOrder() {
+  if (bowlRadio.checked) {
+    console.log("grab a bowl");
+  } else if (burritoRadio.checked) {
+    console.log("warm up a burrito");
+  } else {
+    console.log("you are one of those weird taco people, right");
+  }
+}
