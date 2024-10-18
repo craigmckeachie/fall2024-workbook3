e5439335bd72116c8d14d6f1882e8b44205cd200
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-18 11:13:47 -0400

    object

---
+++ demos/object.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let person = {
+  first: "De'Leon",
+  last: "Williams",
+  location: "Tampa Bay",
+  hairColor: "Black",
+  age: 22,
+};
+
+console.log(person.age);
+
+
+// <div id="messageDiv">Hello</div>
+let messageDiv = {
+  id: "messageDiv",
+  tagName: "DIV",
+  textContent: "Hello",
+  innerText: "Hello",
+};
+
+console.log(messageDiv.innerText);
