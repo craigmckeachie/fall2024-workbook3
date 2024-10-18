8e401c6ff67e985cf1f6b5e3e7c533a81f15b633
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-18 16:00:05 -0400

    car rental

--- exercises/car-rental/scripts/main.js
+++ exercises/car-rental/scripts/main.js
@@ -1,22 +1,56 @@
 "use strict";
 
 //get html elements into variables
-let daysInput = document.getElementById("daysInput");
 let pickupDateInput = document.getElementById("pickupDateInput");
+let daysInput = document.getElementById("daysInput");
+
 let tollCheckbox = document.getElementById("tollCheckbox");
-// let daysInput = {
-//   type: "number",
-//   id: "daysInput",
-//   min: 1,
-//   value: "",
-// };
+let gpsCheckbox = document.getElementById("gpsCheckbox");
+let roadsideCheckbox = document.getElementById("roadsideCheckbox");
+
+let under25YesRadio = document.getElementById("under25YesRadio");
+let under25NoRadio = document.getElementById("under25NoRadio");
+
+let carRentalCostSpan = document.getElementById("carRentalCostSpan");
+let optionsCostSpan = document.getElementById("optionsCostSpan");
+let under25SurchargeCostSpan = document.getElementById("under25SurchargeCostSpan");
+let totalCostSpan = document.getElementById("totalCostSpan");
 
 function estimateCost() {
   //get values out of html input element
-  let numberOfDays = daysInput.value;
+  let numberOfDays = Number(daysInput.value);
   let pickupDate = pickupDateInput.value;
+
   let toll = tollCheckbox.checked;
-  console.log(numberOfDays);
-  console.log(pickupDate);
-  console.log(toll);
+  let gps = gpsCheckbox.checked;
+  let roadside = roadsideCheckbox.checked;
+  let under25 = under25YesRadio.checked;
+
+  let carRentalCost = 0;
+  let optionsCost = 0;
+  let under25Cost = 0;
+  let totalCost = 0;
+
+  carRentalCost = numberOfDays * 29.99;
+  carRentalCostSpan.innerText = carRentalCost;
+
+  if (toll) {
+    optionsCost = optionsCost + 3.95 * numberOfDays;
+  }
+  if (gps) {
+    optionsCost = optionsCost + 2.95 * numberOfDays;
+  }
+  if (roadside) {
+    optionsCost = optionsCost + 2.95 * numberOfDays;
+  }
+
+  optionsCostSpan.innerText = optionsCost.toFixed(2);
+
+  if (under25) {
+    under25Cost = carRentalCost * 0.3;
+    under25SurchargeCostSpan.innerText = under25Cost.toFixed(2);
+  }
+
+  totalCost = carRentalCost + optionsCost + under25Cost;
+  totalCostSpan.innerText = totalCost.toFixed(2);
 }
